8d663d6ccc295d1bc01da2694fc0c51b59d96f03
keikekogake <keike.kogake@gmail.com>
2017-11-17 19:34:50 -0200

    Criação das classes Pessoa.cs e PF.cs

--- Dominio/Pessoa.cs
+++ Dominio/Pessoa.cs
@@ -1,7 +1,20 @@
-namespace Dominio
-{
-    public class Pessoa
-    {
-        
+using System;
+
+namespace Dominio {
+    public class Pessoa {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public Endereco Endereco { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public Pessoa () { }
+
+        public Pessoa (int id, string email, string telefone, Endereco endereco, DateTime dataCadastro) {
+            this.Id = id;
+            this.Email = email;
+            this.Telefone = telefone;
+            this.Endereco = endereco;
+            this.DataCadastro = dataCadastro;
+        }
     }
 }
