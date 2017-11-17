519f9f5230c7f7c530235893697aaa28eda20af9
gabigiraud <gabigiraud@gmail.com>
2017-11-17 19:48:13 -0200

    CPF

--- Util/Validacao.cs
+++ Util/Validacao.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Util
 {
     public class Validacao
@@ -8,7 +10,7 @@ namespace Util
             int[] multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
             string tempCpf, digito;
             int soma = 0, resto = 0;
-            
+
             CPF = CPF.Replace(".", "").Replace("-", "");
 
 
@@ -23,9 +25,35 @@ namespace Util
             {
                 return false;
             }
+            tempCpf = CPF.Substring(0, 9);
+            for (int i = 0; i < 9; i++)
+            {
+                soma += Convert.ToInt16(tempCpf[i].ToString()) * multiplicador1[i];
+            }
+            resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
 
+            digito = resto.ToString();
+            tempCpf = tempCpf + digito;
+            soma = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                soma += Convert.ToInt16(tempCpf[i].ToString()) * multiplicador2[i];
+            }
+            resto = soma % 11;
 
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
 
+            digito = digito + resto.ToString();
+
+            //Verifica se os ultimos 2 digitos obtidos são iguais aos do cpf passado
+            return CPF.EndsWith(digito);
         }
     }
 }
