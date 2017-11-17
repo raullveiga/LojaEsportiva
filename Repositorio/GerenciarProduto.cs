765d4188f79ea0170bc320ecd62176a9e7791a0b
gabigiraud <gabigiraud@gmail.com>
2017-11-17 20:28:54 -0200

    metodo consultar

--- Repositorio/GerenciarProduto.cs
+++ Repositorio/GerenciarProduto.cs
@@ -1,30 +1,58 @@
 using System;
 using System.IO;
+using System.Text;
 using Dominio;
-using Dominio.classes;
+
 
 namespace Repositorio
 {
     public class GerenciarProduto
     {
-       public void Cadastrar(Produto produto){
+        public void Cadastrar(Produto produto)
+        {
             StreamWriter sv = null;
             try
             {
-                sv = new StreamWriter("produtos.csv", true);                
-                sv.WriteLine(produto.Cod + ";" + produto.Nome + ";" + produto.PrecoVenda+ ";" + produto.PrecoCompra+ ";" + produto.Data + ";" + produto.Unidade);
+                sv = new StreamWriter("produtos.csv", true);
+                sv.WriteLine(produto.Cod + ";" + produto.Nome + ";" + produto.PrecoVenda + ";" + produto.PrecoCompra + ";" + produto.Data + ";" + produto.Unidade);
             }
             catch (Exception ex)
             {
                 throw new Exception(ex.Message);
             }
-            finally{
+            finally
+            {
                 sv.Close();
             }
         }
         public string Consultar(string Texto)
         {
-            throw new NotImplementedException();
+            string resultado = "Produto não encontrado";
+            StreamReader ler = null;
+            try
+            {
+                ler = new StreamReader("produto.csv", Encoding.Default);
+                string linha = "";
+                while ((linha = ler.ReadLine()) != null)
+                {
+                    string[] dados = linha.Split(';');
+                    if (dados[0] == Texto)
+                    {
+                        resultado = linha;
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                resultado = "erro ao tentar ler o arquivo. " + ex.Message;
+
+            }
+            finally
+            {
+                ler.Close();
+            }
+            return resultado;
         }
 
         public string Consultar(DateTime Data)
