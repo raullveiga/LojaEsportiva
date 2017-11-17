10dbd1af7636f2105ccc4588fb4415b748a5c84d
Raul <raullalvarenga@hotmail.com>
2017-11-17 21:00:55 -0200

    Criação do método cadastrar da classe Fornecedor

--- Repositorio/Fornecedor.cs
+++ Repositorio/Fornecedor.cs
@@ -7,7 +7,13 @@ namespace Repositorio
     public class Fornecedor : IDominio
     {
 
-        public PJ Empresa { get; set; }
+        public Fornecedor(PJ empresa, string categoria) 
+        {
+            this.Empresa = empresa;
+                this.Categoria = categoria;
+               
+        }
+                public PJ Empresa { get; set; }
         public string Categoria { get; set; }
         
 
@@ -20,12 +26,37 @@ namespace Repositorio
 
         }
 
-        public string Cadastrar()
+        public string Cadastrar(PJ empresa)
+        {
+            string rt = "";
+
+            StreamWriter sv = null;
+
+            try
+            {
+                sv = new StreamWriter("produtos.csv", true);                
+                sv.WriteLine(   empresa.Id+";"+empresa.RazaoSocial+";"+empresa.Email+";"+empresa.Telefone+";"+empresa.DataCriacao+";"+
+                                empresa.Endereco.Logradouro+";"+empresa.Endereco.Numero+";"+empresa.Endereco.Complemento+";"+empresa.Endereco.Bairro+";"+
+                                empresa.Endereco.Cidade+";"+empresa.Endereco.Uf+";"+DateTime.Now.ToShortDateString());
+                rt = "Fornecedor cadastrado com sucesso!";
+            }
+            catch (Exception ex)
+            {
+               rt = "Erro ao tentar cadastrar"+ex.Message;
+                
+            }
+            finally{
+                sv.Close();
+            }
+            return rt;
+        }
+
+        public string Consultar()
         {
             throw new NotImplementedException();
         }
 
-        public string Consultar()
+        public string Cadastrar()
         {
             throw new NotImplementedException();
         }
