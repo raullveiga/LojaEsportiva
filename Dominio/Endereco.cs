6a7f6b3b42baad6399a6c87e875f75e5a63b8899
Raul <raullalvarenga@hotmail.com>
2017-11-17 20:25:13 -0200

    aer

--- Dominio/Endereco.cs
+++ Dominio/Endereco.cs
@@ -1,9 +1,7 @@
 namespace Dominio.classes
 {
     public class Endereco
-    {
-
-        
+    {   
         public string Logradouro { get; set; }
         public string Numero { get; set; }
         public string Complemento { get; set; }
