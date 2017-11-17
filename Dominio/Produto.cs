cc570f073bdebf6a622e13244976402586294f68
Paulo Brandão <32689830+paulobrandaooficial@users.noreply.github.com>
2017-11-17 20:29:23 -0200

    Classes comentadas

--- Dominio/Produto.cs
+++ Dominio/Produto.cs
@@ -1,7 +1,10 @@
 using System;
 
-namespace Dominio.classes
+namespace Dominio
 {
+    /// <summary>
+    /// Classe para guardar produtos
+    /// </summary>
     public class Produto
     {
 
@@ -12,10 +15,22 @@ namespace Dominio.classes
         public string Unidade { get; set; }
         public DateTime Data { get; set; }
 
+        /// <summary>
+        /// Método vazio
+        /// </summary>
         public Produto()
         {
         }
 
+        /// <summary>
+        /// Método com parâmetros para guardar o produto
+        /// </summary>
+        /// <param name="cod">Código do produto em texto</param>
+        /// <param name="nome">Nome do produto em texto</param>
+        /// <param name="precoVenda">Preço de venda em double</param>
+        /// <param name="precoCompra">Preço de compra em double</param>
+        /// <param name="unidade">Unidade de medidas</param>
+        /// <param name="data">Data de Cadastro</param>
         public Produto(string cod , string nome, double precoVenda, double precoCompra, string unidade, DateTime data)
         {   
             this.Cod = cod;
@@ -23,6 +38,7 @@ namespace Dominio.classes
             this.PrecoVenda = precoVenda;
             this.PrecoCompra = precoCompra;
             this.Unidade = unidade;
+            this.Data = data;
 
         }
         
