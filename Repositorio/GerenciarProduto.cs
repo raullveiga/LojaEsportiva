aa257083fa3f2d90b9fac623655c3e8d4f18646b
Paulo Brandão <32689830+paulobrandaooficial@users.noreply.github.com>
2017-11-17 19:45:26 -0200

    Classes Criadas + Interface

--- Repositorio/GerenciarProduto.cs
+++ Repositorio/GerenciarProduto.cs
@@ -1,7 +1,36 @@
+using System;
+using System.IO;
+using Dominio;
+using Dominio.classes;
+
 namespace Repositorio
 {
     public class GerenciarProduto
     {
-        
+       public void Cadastrar(Produto produto){
+            StreamWriter sv = null;
+            try
+            {
+                sv = new StreamWriter("produtos.csv", true);                
+                sv.WriteLine(produto.Cod + ";" + produto.Nome + ";" + produto.PrecoVenda+ ";" + produto.PrecoCompra+ ";" + produto.Data + ";" + produto.Unidade);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally{
+                sv.Close();
+            }
+        }
+        public string Consultar(string Texto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Consultar(DateTime Data)
+        {
+            throw new NotImplementedException();
+        }
     }
+
 }
