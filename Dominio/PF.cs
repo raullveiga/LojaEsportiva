db766bb15c469dbc924d86e3c44441016671c871
keikekogake <keike.kogake@gmail.com>
2017-11-21 19:41:22 -0200

    Criação do sumário das classes PF.cs, PJ.cs e Cliente.cs

--- Dominio/PF.cs
+++ Dominio/PF.cs
@@ -1,12 +1,29 @@
 using System;
 
 namespace Dominio {
+    /// <summary>
+    /// Classe que recebe as informações do cliente PF
+    /// </summary>
     public class PF : Pessoa {
         public string Cpf { get; set; }
         public string Nome { get; set; }
         public DateTime DataNascimento { get; set; }
-        public PF () { }
 
+        /// <summary>
+        /// Método construtor sem parametros;
+        /// </summary>
+        public PF () { }
+        
+        /// <summary>
+        /// Método construtor que recebe como parametro os dados do Cliente PF;
+        /// </summary>
+        /// <param name="id">Código do cliente tipo Inteiro</param>
+        /// <param name="nome">Nome do cliente tipo String</param>
+        /// <param name="cpf">CPF do cliente tipo String</param>
+        /// <param name="email">Email do cliente tipo String</param>
+        /// <param name="telefone">Telefone do cliente tipo String</param>
+        /// <param name="dataNascimento">Data de Nascimento  do cliente tipo DateTime</param>
+        /// <param name="endereco">Endereço do cliente tipo Endereco</param>
         public PF (int id, string nome, string cpf, string email, string telefone, DateTime dataNascimento, Endereco endereco) {
             this.Id = id;
             this.Nome = nome;
