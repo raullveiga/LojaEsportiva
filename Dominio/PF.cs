2cbe5a4592585474ad64feaf295e52c59db45fd9
Paulo Brandão <32689830+paulobrandaooficial@users.noreply.github.com>
2017-11-21 18:44:02 -0200

    Cadastro ok

--- Dominio/PF.cs
+++ Dominio/PF.cs
@@ -1,7 +1,7 @@
 using System;
 
 namespace Dominio {
-    public class PF {
+    public class PF  : Pessoa {
         public string Cpf { get; set; }
         public string Nome { get; set; }
         public DateTime DataNascimento { get; set; }
