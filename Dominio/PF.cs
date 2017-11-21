213ab2c086e085ff7e10d80eae30ce3700901d3b
keikekogake <keike.kogake@gmail.com>
2017-11-21 18:32:06 -0200

    Correção da classe Cliente.cs e PF.cs

--- Dominio/PF.cs
+++ Dominio/PF.cs
@@ -1,18 +1,20 @@
 using System;
 
 namespace Dominio {
-    public class PF {
+    public class PF : Pessoa {
         public string Cpf { get; set; }
         public string Nome { get; set; }
         public DateTime DataNascimento { get; set; }
-        public Pessoa Pessoa { get; set; }
         public PF () { }
 
-        public PF (string cpf, string nome, DateTime dataNascimento, Pessoa pessoa) {
-            this.Cpf = cpf;
+        public PF (int id, string nome, string cpf, string email, string telefone, DateTime dataNascimento, Endereco endereco) {
+            this.Id = id;
             this.Nome = nome;
+            this.Cpf = cpf;
+            this.Email = email;
+            this.Telefone = telefone;
             this.DataNascimento = dataNascimento;
-            this.Pessoa = pessoa;
+            this.Endereco = endereco;
 
         }
 
